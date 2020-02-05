260298c9fade3c7f600d0fbbcd538484f892c1ae
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-02-05 14:01:34 -0800

    complete passport token authentication

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
 const Authentication = require('./controllers/authentication')
+const passportService = require('./services/passport')
+const passport = require('passport')
+
+// Use jwt tokens and not cookies
+const requireAuth = passport.authenticate('jwt', { session: false })
 
 module.exports = function(app) {
+    app.get('/', requireAuth, (req, res) => {
+        res.send({ hi: 'there' })
+    })
     app.post('/signup', Authentication.signup)
 }
