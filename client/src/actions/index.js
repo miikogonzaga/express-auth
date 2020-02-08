bfab2376c7a09e3d136ced55812d33de2d718ef6
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-02-08 08:03:06 -0800

    add user signin

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,6 +12,17 @@ export const signup = (formProps, callback) => async dispatch => {
     }
 }
 
+export const signin = (formProps, callback) => async dispatch => {
+    try {
+        const response = await axios.post('http://localhost:8000/signin', formProps)
+        dispatch({ type: AUTH_USER, payload: response.data.token })
+        localStorage.setItem('token', response.data.token)
+        callback()
+    } catch (e) {
+        dispatch({ type: AUTH_ERROR, payload: 'Invalid login credentials' })
+    }
+}
+
 export const signout = () => {
     // Clear local storage
     localStorage.removeItem('token')
